a9906221d9ec6c95bc403ad7a24d5827afc5ff42
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-11-12 16:18:57 -0600

    Create package-info.java

---
+++ src/io/github/tobiasbriones/ep/factura/ui/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Tobias Briones.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Contains the UI package to present the app.
+ */
+
+package io.github.tobiasbriones.ep.factura.ui;
