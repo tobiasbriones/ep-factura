1ca5b954968fe2cc0cfefb0a82fa0d0b33ecef1c
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-11-13 18:54:35 -0600

    AppProductRepository implemented

--- src/io/github/tobiasbriones/ep/factura/repository/AppProductRepository.java
+++ src/io/github/tobiasbriones/ep/factura/repository/AppProductRepository.java
@@ -7,6 +7,7 @@
 
 package io.github.tobiasbriones.ep.factura.repository;
 
+import io.github.tobiasbriones.ep.factura.data.ProductDao;
 import io.github.tobiasbriones.ep.factura.domain.model.product.IdProductAccessor;
 import io.github.tobiasbriones.ep.factura.domain.model.product.Product;
 import io.github.tobiasbriones.ep.factura.domain.repository.ProductRepository;
@@ -16,33 +17,35 @@ import java.util.Optional;
 
 public final class AppProductRepository implements ProductRepository {
 
+    private final ProductDao productDao;
 
-    public AppProductRepository() {
+    public AppProductRepository(ProductDao productDao) {
+        this.productDao = productDao;
     }
 
     @Override
     public Optional<Product> get(IdProductAccessor id) {
-        return Optional.empty();
+        return productDao.fetch(id);
     }
 
     @Override
     public List<Product> getAll(int page, int pageSize) {
-        return null;
+        return productDao.fetchAll(page, pageSize);
     }
 
     @Override
     public void add(Product record) {
-
+        productDao.create(record);
     }
 
     @Override
     public void set(Product record) {
-
+        productDao.update(record);
     }
 
     @Override
     public void remove(Product record) {
-
+        productDao.delete(record);
     }
 
 }
