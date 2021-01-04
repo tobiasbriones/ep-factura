070118c1a331b850fadd623d728b3fc317b321a6
Tobias Briones <tobiasbrionesdev@gmail.com>
2021-01-03 22:11:50 -0600

    Define CityDao

---
+++ src/io/github/tobiasbriones/ep/factura/data/CityDao.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Factura.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package io.github.tobiasbriones.ep.factura.data;
+
+import io.github.tobiasbriones.ep.factura.domain.model.city.City;
+
+import java.util.List;
+
+public interface CityDao {
+
+    List<City> fetchAll();
+
+}
