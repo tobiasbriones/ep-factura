d4495bac9268d5139c589bcabeea9337f8d55b67
Tobias Briones <tobiasbrionesdev@gmail.com>
2021-01-03 22:09:37 -0600

    Add Community model

---
+++ src/io/github/tobiasbriones/ep/factura/domain/model/city/community/Community.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Factura.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package io.github.tobiasbriones.ep.factura.domain.model.city.community;
+
+public final class Community implements CommunityModel {
+
+    private final String name;
+
+    public Community(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Community[" +
+               "name=" + name + ", " +
+               "]";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+}
