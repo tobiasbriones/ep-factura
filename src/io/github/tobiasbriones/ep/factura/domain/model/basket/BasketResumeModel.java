6788209c00d44103513edfa113277e8abc609bf6
Tobias Briones <tobiasbrionesdev@gmail.com>
2021-01-05 13:34:43 -0600

    Define BasketResumeModel

---
+++ src/io/github/tobiasbriones/ep/factura/domain/model/basket/BasketResumeModel.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Factura.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package io.github.tobiasbriones.ep.factura.domain.model.basket;
+
+public interface BasketResumeModel {
+
+    double getIsv();
+
+    double getSubtotal();
+
+    double getTotal();
+
+}
