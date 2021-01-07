d6648b2399e2a3e2bc4f2360dbffa516a2af7153
Tobias Briones <tobiasbrionesdev@gmail.com>
2021-01-07 14:55:19 -0600

    Implement Print component from Summary component

---
+++ src/io/github/tobiasbriones/ep/factura/ui/mainbilling/print/Print.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2020 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Factura.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package io.github.tobiasbriones.ep.factura.ui.mainbilling.print;
+
+import io.github.tobiasbriones.ep.factura.domain.model.basket.BasketModel;
+import io.github.tobiasbriones.ep.factura.ui.core.SwingComponent;
+import io.github.tobiasbriones.ep.factura.ui.core.rx.Observable;
+
+import javax.swing.*;
+
+public final class Print implements SwingComponent<JPanel> {
+
+    public interface Output {
+
+        void onPrint();
+
+        void onPrintWithNewCustomer();
+
+    }
+
+    public static Print newInstance() {
+        final var component = new Print();
+
+        component.init();
+        return component;
+    }
+
+    private final PrintController controller;
+    private final PrintView view;
+
+    private Print() {
+        this.controller = new PrintController();
+        this.view = new PrintView(controller);
+    }
+
+    @Override
+    public JPanel getViewComponent() {
+        return view.getViewComponent();
+    }
+
+    public void setOutput(Print.Output output) {
+        controller.setOutput(output);
+    }
+
+    private void init() {
+        view.init();
+        initController();
+    }
+
+    private void initController() {
+        controller.setView(view);
+        controller.init();
+    }
+
+}
