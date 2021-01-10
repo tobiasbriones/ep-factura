058c68dc47e1245323cbb7767b43d34831c61509
Tobias Briones <tobiasbrionesdev@gmail.com>
2021-01-09 22:53:50 -0600

    Create Printer

--- src/io/github/tobiasbriones/ep/factura/ui/mainbilling/MainBillingWindow.java
+++ src/io/github/tobiasbriones/ep/factura/ui/mainbilling/MainBillingWindow.java
@@ -16,10 +16,10 @@ import io.github.tobiasbriones.ep.factura.data.CityDao;
 import io.github.tobiasbriones.ep.factura.data.CommunityDao;
 import io.github.tobiasbriones.ep.factura.data.ProductDao;
 import io.github.tobiasbriones.ep.factura.domain.model.basket.BasketModel;
-import io.github.tobiasbriones.ep.factura.domain.model.bill.BillAccessor;
 import io.github.tobiasbriones.ep.factura.domain.model.bill.BillModel;
 import io.github.tobiasbriones.ep.factura.domain.model.bill.BillMutator;
-import io.github.tobiasbriones.ep.factura.domain.model.customer.CustomerNameAccessor;
+import io.github.tobiasbriones.ep.factura.domain.usecase.PrintBillUseCase;
+import io.github.tobiasbriones.ep.factura.io.Printer;
 import io.github.tobiasbriones.ep.factura.ui.core.SwingComponent;
 import io.github.tobiasbriones.ep.factura.ui.mainbilling.customer.CustomerCreationDialog;
 import io.github.tobiasbriones.ep.factura.ui.mainbilling.header.Header;
@@ -48,21 +48,6 @@ public final class MainBillingWindow implements SwingComponent<JFrame> {
 
         component.init();
         return component;
-    }
-
-    private static String createPrintTitle(CustomerNameAccessor customer) {
-        return "Factura - " + customer.getName();
-    }
-
-    private static String createPrintMsg(BillAccessor bill) {
-        final BasketModel basket = bill.getBasket();
-        final int numberOfProducts = getNumberOfProducts(basket);
-        final double total = bill.getTotal();
-        return numberOfProducts + " productos. Total: $" + total;
-    }
-
-    private static int getNumberOfProducts(BasketModel basket) {
-        return basket.size();
     }
 
     // Again, this would be a record class in Java 17+
@@ -189,12 +174,12 @@ public final class MainBillingWindow implements SwingComponent<JFrame> {
         controller.init();
     }
 
-    private void showBillPrintedDialog(BillAccessor bill) {
+    private void showBillPrintedDialog(BillModel bill) {
         final JFrame parent = getViewComponent();
-        final String msg = createPrintMsg(bill);
-        final String title = createPrintTitle(bill.getCustomer());
-        final int type = JOptionPane.INFORMATION_MESSAGE;
-        JOptionPane.showMessageDialog(parent, msg, title, type);
+        final var printer = new Printer(parent);
+        final var printUseCase =new PrintBillUseCase(bill);
+
+        printUseCase.execute(printer);
     }
 
     // This shouldn't go here. I put it because this example app ends right
